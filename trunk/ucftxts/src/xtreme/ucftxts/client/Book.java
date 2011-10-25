be4f1124807b9c4974e4f5cce0a946f4b1463eb2
Scherererer@gmail.com <Scherererer@gmail.com@84041e3b-1121-c22b-5057-d4d1213c1c90>
2011-10-25 02:44:56 +0000

    Initial code commit

---
+++ trunk/ucftxts/src/xtreme/ucftxts/client/Book.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package xtreme.ucftxts.client;
+
+import xtreme.ucftxts.client.Retailer;
+import java.util.*;
+
+/**
+ * @author Michael Scherer
+ *	Class for holding information about a book, including author,
+ *	ISBN, title, abstract, and prices
+ */
+public class Book
+{
+	public Book(long isbn, String author, String title, String _abstract)
+	{
+		this.mISBN = isbn;
+		this.mAuthor = author;
+		this.mTitle = title;
+		this.mAbstract = _abstract;
+		
+		this.mRetailers = new Vector<Retailer>();
+	}
+	
+	public boolean AddRetailer(Retailer retailer)
+	{
+		return mRetailers.add(retailer);
+	}
+	
+	public String GetAuthor()
+	{
+		return mAuthor;
+	}
+	
+	public void SetAuthor(String author)
+	{
+		mAuthor = author;
+	}
+	
+	public String GetTitle()
+	{
+		return mTitle;
+	}
+	
+	public void SetTitle(String title)
+	{
+		mTitle = title;
+	}
+	
+	public String GetAbstract()
+	{
+		return mAbstract;
+	}
+	
+	public void SetAbstract(String _abstract)
+	{
+		mAbstract = _abstract;
+	}
+	
+	public long GetISBN()
+	{
+		return mISBN;
+	}
+	
+	public void SetISBN(long isbn)
+	{
+		mISBN = isbn;
+	}
+	
+	private String mAuthor;
+	private String mTitle;
+	private String mAbstract;
+	private long mISBN;
+	
+	private Vector<Retailer> mRetailers;
+}
