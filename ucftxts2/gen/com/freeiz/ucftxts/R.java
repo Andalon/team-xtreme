1e01dc413f499aab8ec4098752b3a0614b23d0e5
crimsoncoyote@gmail.com <crimsoncoyote@gmail.com@84041e3b-1121-c22b-5057-d4d1213c1c90>
2011-11-18 03:42:51 +0000

    Beginning interaction with query

--- ucftxts2/gen/com/freeiz/ucftxts/R.java
+++ ucftxts2/gen/com/freeiz/ucftxts/R.java
@@ -17,17 +17,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060004;
-        public static final int button2=0x7f060005;
-        public static final int editText1=0x7f060000;
-        public static final int editText2=0x7f060001;
-        public static final int editText3=0x7f060002;
-        public static final int editText4=0x7f060003;
-        public static final int textView1=0x7f060007;
-        public static final int textView2=0x7f060006;
+        public static final int button1=0x7f060007;
+        public static final int editText1=0x7f060001;
+        public static final int editText2=0x7f060002;
+        public static final int editText3=0x7f060003;
+        public static final int editText4=0x7f060004;
+        public static final int progressBar1=0x7f060000;
+        public static final int scanButton=0x7f060005;
+        public static final int searchButton=0x7f060006;
+        public static final int textView1=0x7f060009;
+        public static final int textView2=0x7f060008;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int loading=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- ucftxts2/src/com/freeiz/ucftxts/Ucftxts2Activity.java
+++ ucftxts2/src/com/freeiz/ucftxts/Ucftxts2Activity.java
@@ -2,12 +2,55 @@ package com.freeiz.ucftxts;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Ucftxts2Activity extends Activity {
+public class Ucftxts2Activity extends Activity implements OnClickListener 
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button searchButton = (Button)findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(this);
+        
+        Button scanButton = (Button)findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(this);
+    }
+    
+    public void onClick(View v)
+    {
+    	EditText isbn = (EditText)findViewById(R.id.editText1);
+    	EditText title = (EditText)findViewById(R.id.editText2);
+    	EditText author = (EditText)findViewById(R.id.editText3);
+    	EditText topic = (EditText)findViewById(R.id.editText4);
+    	if(v.equals((Button)findViewById(R.id.searchButton)))
+    	{
+    		waitForQuery();
+    		isbn.setText("done waiting");
+    	}
+    	else if(v.equals((Button)findViewById(R.id.scanButton)))
+    	{
+    		///////////////
+    		//Scan Barcode
+    		//Fire Intent to Barcode Scanner here
+    		///////////////
+    	}
+    	
+    }
+    
+    public boolean waitForQuery()
+    {
+    	//onPause();
+    	setContentView(R.layout.loading);
+    	
+    	boolean doneWaiting = false;
+    	return true;
     }
 }
