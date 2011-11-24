ae73170fa1d443f91d0615a83bbf2b6cdfae6354
crimsoncoyote@gmail.com <crimsoncoyote@gmail.com@84041e3b-1121-c22b-5057-d4d1213c1c90>
2011-11-24 03:36:07 +0000

    spelling error. derp

--- trunk/ucftxts2/src/com/freeiz/ucftxts/ResultsActivity.java
+++ trunk/ucftxts2/src/com/freeiz/ucftxts/ResultsActivity.java
@@ -3,8 +3,10 @@ package com.freeiz.ucftxts;
 import java.util.Vector;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.freeiz.ucftxts.client.Book;
 import com.freeiz.ucftxts.client.XtremeResponseHandler;
@@ -13,6 +15,18 @@ import com.freeiz.ucftxts.client.XtremeResponseHandler;
 public class ResultsActivity extends Activity implements OnClickListener, XtremeResponseHandler
 {
 
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button searchButton = (Button)findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(this);
+        
+        Button scanButton = (Button)findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(this);
+	}
+	
 	@Override
 	public void XtremeResponse(Vector<Book> books) 
 	{
