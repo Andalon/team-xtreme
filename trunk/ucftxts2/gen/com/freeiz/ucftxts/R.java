1621f29880edecb45bdf2d5227416fe71bffb612
crimsoncoyote@gmail.com <crimsoncoyote@gmail.com@84041e3b-1121-c22b-5057-d4d1213c1c90>
2011-11-29 05:26:26 +0000

    holy crap it works mostly

--- trunk/ucftxts2/gen/com/freeiz/ucftxts/R.java
+++ trunk/ucftxts2/gen/com/freeiz/ucftxts/R.java
@@ -17,7 +17,6 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int addBtn=0x7f06000e;
         public static final int button1=0x7f060007;
         public static final int editText1=0x7f060001;
         public static final int editText2=0x7f060002;
@@ -27,9 +26,9 @@ public final class R {
         public static final int popup_layout=0x7f06000a;
         public static final int popup_title=0x7f06000b;
         public static final int progressBar1=0x7f060000;
+        public static final int results_layout=0x7f06000d;
         public static final int scanButton=0x7f060005;
         public static final int searchButton=0x7f060006;
-        public static final int tableLayout1=0x7f06000d;
         public static final int textView1=0x7f060009;
         public static final int textView2=0x7f060008;
     }
